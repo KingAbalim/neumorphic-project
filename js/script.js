80ed4f6f3a226cf615c26ecf3b2fb400553879cf
Bruno <brunofernando@tragodiedesign.com.br>
2021-07-18 21:56:20 +0000

    Adicionados os css e js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const inputs = document.getElementById(".input");
+
+function addBoxshadow(){
+('box-shadow:  inset 8px 8px 8px #cbced1,-8px -8px 8px #ffffff');
+}
+inputs(input=>{
+    input.addEventListener('addBoxshadow');
+});
